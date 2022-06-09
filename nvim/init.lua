89205b992a07331d3d7799e1170165dc1f30c610
eeue <105617269+eeue@users.noreply.github.com>
2022-06-09 08:24:29 -0400

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,11 @@ lua = require('plugins')
 local lsp = require "lspconfig"
 local coq = require "coq"
 require("nvim-autopairs").setup {}
+require('lualine').setup()
 
 vim.cmd('COQnow -s')
 vim.cmd('colorscheme dracula')
+
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.mouse = "a"
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,44 @@
-local use = require('packer').use
-return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use 'neovim/nvim-lspconfig'
-	use 'ms-jpq/coq_nvim'
-	use 'ms-jpq/coq.artifacts'
-	use 'windwp/nvim-autopairs'
-	use 'nvim-telescope/telescope.nvim'
-	use 'nvim-lua/plenary.nvim'
-	use 'dense-analysis/ale'
-	use 'dracula/vim'
-end)
+local fn = vim.fn
 
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local use = require("packer").use
+
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim") -- let packer do cool stuff
+	use("neovim/nvim-lspconfig") -- enable LSP
+	use("ms-jpq/coq_nvim") -- fast as FUUUUUUUUUCK completion.
+	use("ms-jpq/coq.artifacts") -- lots of snippets
+	use("windwp/nvim-autopairs") -- autopair
+	use("nvim-lua/plenary.nvim") -- useful functions
+	use("dense-analysis/ale") -- formatters, linters, lsp shit
+	use("dracula/vim") -- colorscheme
+	use("nvim-lualine/lualine.nvim") -- cute line
+	use("kyazdani42/nvim-web-devicons") -- cool icons
+end),
+	
+use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
